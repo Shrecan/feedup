ead99c7fe4744ee9a373dacab6dfa69d74f2e25a
Shreeraj Jadhav <shreerajjadhav404@gmail.com>
2026-06-14 08:37:49 +0530

    Update generate.js

--- src/main/resources/static/js/generate.js
+++ src/main/resources/static/js/generate.js
@@ -1,30 +1,9 @@
-// const generatedLink = sessionStorage.getItem("generatedLink") || "";
-// const input = document.getElementById("generatedLink");
-// input.value = generatedLink;
+const generatedLink = sessionStorage.getItem("generatedLink") || "";
+const input = document.getElementById("generatedLink");
+input.value = generatedLink;
 
-// document.getElementById("copyLinkBtn").addEventListener("click", async () => {
-//     await navigator.clipboard.writeText(input.value);
-//     document.getElementById("copyLinkBtn").textContent = "Copied";
-// });
-document.getElementById("copyLinkBtn").addEventListener("click", () => {
-    const text = document.getElementById("generatedLink").value;
-
-    if (navigator.clipboard && window.isSecureContext) {
-        navigator.clipboard.writeText(text).then(() => {
-            document.getElementById("copyLinkBtn").textContent = "Copied";
-        }).catch(fallbackCopy);
-    } else {
-        fallbackCopy(text);
-    }
+document.getElementById("copyLinkBtn").addEventListener("click", async () => {
+    await navigator.clipboard.writeText(input.value);
+    document.getElementById("copyLinkBtn").textContent = "Copied";
 });
 
-function fallbackCopy(text) {
-    const tempInput = document.createElement("textarea");
-    tempInput.value = text;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-
-    document.getElementById("copyLinkBtn").textContent = "Copied";
-}
